a34b3b73eb756692eced6b689cadebbd86a0aab6
root <okekefeargod@gmail.com>
2024-02-05 04:48:17 -0800

    correcting the stats endpoint and adding uniitests for count and get methods

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ This module defines view functions related to the index of the API.
 """
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, Flask, Blueprint
 from models import storage
 
 
@@ -18,7 +18,7 @@ hbnb_dict = {
 }
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False)
 def status():
     """
     Return the status of the API.
@@ -34,10 +34,14 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """Retrieve the number of each object by type"""
     return_dict = {}
     for key, value in hbnb_dict.items():
         return_dict[key] = storage.count(value)
     return jsonify(return_dict)
+
+
+if __name__ == "__main__":
+    pass
